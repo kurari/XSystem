cba6d8ef14e6b6d049a742476f16ff935b64a42a
Hajime MATSUMOTO <hajime@avap.co.jp>
2010-09-13 05:24:37 +0800

    Test Script Update

--- bin/test.php
+++ bin/test.php
@@ -1,18 +1,9 @@
 <?php
 require_once dirname(__FILE__).'/../script/bin.header.inc.php';
+require_once 'test/runner.class.php';
 
-$target = $argv[1];
-$pos = strrpos(basename($target), '.');
-$class = ucfirst(substr(basename($target), 0, $pos)).'Test';
-require_once $target;
-
-try {
-	$TestCase = new $class( );
-	$TestCase->init( );
-	$TestCase->run( );
-}catch(Exception $e){
-	echo $e->getMessage();
-	echo $e->getFile();
-	echo $e->getLine();
-}
+$all = array_slice($argv, 1);
+$Runner = new XTestRunner( );
+foreach($all as $t ) $Runner->addCase( $t );
+$Runner->run( );
 ?>
